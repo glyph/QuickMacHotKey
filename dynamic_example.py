ae19a626ca3c9fd38c93c6b19ef667603d804ef2
Glyph <code@glyph.im>
2023-10-26 12:25:19 -0700

    dynamic example
    This commit was sponsored by Jason Walker, Matt Campbell, and Sergio
    Bost, and my other patrons.  If you want to join them, you can support
    my work at https://patreon.com/creatorglyph.

---
+++ dynamic_example.py
@@ -0,0 +1,26 @@
+
+from pathlib import Path
+
+from AppKit import NSApplication
+from PyObjCTools import AppHelper
+
+from quickmachotkey import mask, quickHotKey
+from quickmachotkey.configurators.jsonfile import JSONFileConfigurator
+from quickmachotkey.constants import (cmdKey, controlKey, kVK_ANSI_J,
+                                      kVK_ANSI_K, shiftKey)
+
+
+config = JSONFileConfigurator(Path("whatkey.json"))
+@quickHotKey(
+    virtualKey=kVK_ANSI_J,
+    modifierMask=mask(cmdKey, controlKey),
+    configurator=config,
+)
+def handler() -> None:
+    print("handled, switching to K")
+    handler.configure(virtualKey=kVK_ANSI_K, modifierMask=mask(cmdKey, shiftKey))
+
+
+if __name__ == "__main__":
+    NSApplication.sharedApplication()
+    AppHelper.runEventLoop()
