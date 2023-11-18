dc7a99ba57575ec08eb4614a93fed7b8169541f8
Glyph <code@glyph.im>
2023-11-17 20:22:05 -0800

    explain what's going on here
    This commit was sponsored by Matt Campbell, Jason Walker, Sergio Bost,
    and my other patrons.  If you want to join them, you can support my
    work at https://patreon.com/creatorglyph.

--- src/quickmachotkey/_MinimalHIToolbox/__init__.py
+++ src/quickmachotkey/_MinimalHIToolbox/__init__.py
@@ -1,7 +1,24 @@
 """
-The only documentation of this functionality is to be found in
+These APIs are still present in modern macOS (i.e. macOS 14, Sonoma), but they
+are part of a deprecated framework (Carbon) which has been otherwise removed.
+Thus although the functions defined within remain the only supported way to
+register hot keys without special permission (that is to say, C{CGEventTap},
+with accessibility permissions), their documentation is not present in the main
+apple documentation and pyObjC does not wrap the framework.
 
-/System/Library/Frameworks/Carbon.framework/Versions/A/Frameworks/HIToolbox.framework/Versions/A/Headers/Events.h
+So, erstwhile maintainers, here are the current relevant references.  The first
+few are on your local filesystem if you have Xcode installed:
+
+/System/Library/Frameworks/Carbon.framework/Versions/A/Frameworks/HIToolbox.framework/Versions/A/Resources/BridgeSupport/HIToolbox.bridgesupport
+
+/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/Carbon.framework/Versions/A/Frameworks/HIToolbox.framework/Versions/A/Headers/CarbonEventsCore.h
+
+/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/Carbon.framework/Versions/A/Frameworks/HIToolbox.framework/Versions/A/Headers/CarbonEvents.h
+
+And finally there is a bit of conceptual documentation in apple's document
+I{archive}, even if it's not present in the current active documentation:
+
+https://developer.apple.com/library/archive/documentation/Carbon/Conceptual/Carbon_Event_Manager/Tasks/CarbonEventsTasks.html
 """
 
 import objc as _objc  # type:ignore[import]
